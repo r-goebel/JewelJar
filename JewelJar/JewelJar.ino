58325e41d4777362b4aa1a7a05f0408a64e7d92f
Rebecca Goebel <rebecca.goebel@fgdo.de>
2019-12-08 13:53:47 +0100

    renamed files

---
+++ JewelJar/JewelJar.ino
@@ -0,0 +1,28 @@
+//Board: Wemos D1 R2 & mini
+//Upload speed:115200
+
+#include "Effects.h"
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PixelPin D2
+int NUMPIXELS = 7;
+
+Effects jewel = Effects(NUMPIXELS, PixelPin, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(115200);
+
+  jewel.begin();
+  jewel.setBrightness(10);
+  jewel.rainbowCycle(10);
+
+  jewel.show();
+}
+
+void loop() {
+
+  jewel.Update();
+  
+}
