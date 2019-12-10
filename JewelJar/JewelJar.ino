cf5949bd8788826234aa82f97b87950a4de44353
Rebecca Goebel <rebecca.goebel@fgdo.de>
2019-12-10 10:15:33 +0100

    setup code für jewel jar

--- JewelJar/JewelJar.ino
+++ JewelJar/JewelJar.ino
@@ -11,18 +11,70 @@ int NUMPIXELS = 7;
 
 Effects jewel = Effects(NUMPIXELS, PixelPin, NEO_GRB + NEO_KHZ800);
 
+//Definition of possible effects
+char* effectList[] = {"fade red", "fade cyan", "fade green", "rainbow cycle"};
+int NumberEffects = 4;
+int SelectedNew = random(0,NumberEffects);    //initial definition of new selected effect
+int Selected;
+bool EffectChange = 1;                        //variable indication change in effect (0=no change, 1=change), initially one change necessary 
+
+
+//Definition of Switch
+#define PinSwitch D6
+int intervalSwitch = 500; //interval in which no new Switch is allowed
+int lastSwitch;
+
 void setup() {
   Serial.begin(115200);
 
   jewel.begin();
-  jewel.setBrightness(200);
-  jewel.rainbowCycle(10);
+  jewel.setBrightness(150);
 
   jewel.show();
 }
 
 void loop() {
 
-  jewel.Update();
+  //if switch state is HIGH, Change Effect
+  if (millis()-lastSwitch > intervalSwitch){
+    if (digitalRead(PinSwitch) == HIGH){
+      EffectChange = 1;
+      if (Selected < NumberEffects-1){
+        SelectedNew = Selected+1;
+      } else {
+        SelectedNew = 0;
+      }
+      lastSwitch = millis();
+    }
+  }
+
+  if (EffectChange == 1){
+    Serial.println("EffectChange noticed");
+    Selected = SelectedNew;
+    EffectChange = 0;
+    jewel.clear();
+    //Initialization
+    SelectEffect();
+  }
+  //no Change: just update effect
+  else{
+    jewel.Update();
+  }
   
+}
+
+
+void SelectEffect (){
+  if (Selected == 0){ //fade red
+    jewel.FadeInOut(jewel.Color(255,0,0),10); 
+    
+  } else if (Selected == 1){ //fade cyan
+    jewel.FadeInOut(jewel.Color(0,255,255),10); 
+    
+  } else if (Selected == 2){ //fade green
+    jewel.FadeInOut(jewel.Color(0,255,0),10); 
+    
+  } else if (Selected == 3){ //RainbowCycleAll
+    jewel.rainbowCycleAll(50);
+  }    
 }
